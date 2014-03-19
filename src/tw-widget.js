760368e68b07bc0f775bb6cb482036a14e4520ff
edencore <edencore@gmail.com>
2014-03-19 18:56:15 +0400

    Styles and adding some elements

--- src/tw-widget.js
+++ src/tw-widget.js
@@ -3,11 +3,34 @@
  * @author eden lane
  * @require jQuery
  */
-
+$(document).ready(function(){
 (function ($) {
-  $(".twitch-widget").each(function () {
-    var $w = $(this),
-        channel = $w.attr("data-twitch-channel");
+  var $all = $(".tw-widget");
+  //console.log($all);
 
+  $all.each(function () {
+    console.log("each all")
+    var $w = $(this),
+        name = $w.attr("data-channel-name"),
+        $title, $count, $header, $img;
+
+    console.log(channel);
+
+    // channel image
+    $img = $(document.createElement('img'))
+      .attr("width", "40px")
+      .attr("height", "40px")
+      .attr("src","http://static-cdn.jtvnw.net/jtv_user_pictures/barreljumpers-profile_image-a9173587c121f8ed-300x300.png");
+
+    // channel title
+    $title = $(document.createElement('a'))
+      .addClass('tw-title')
+      .text("Barrel Jumpers");
+
+    // game name
+    $w.append($img).append($title);
   })
 })(jQuery);
+
+
+});
