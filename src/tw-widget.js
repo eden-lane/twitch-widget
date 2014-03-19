f694cd5dfbe08163ba11a2cdc28d325c2f7db44e
edencore <edencore@gmail.com>
2014-03-19 16:07:50 +0400

    added page for test and style file

--- src/tw-widget.js
+++ src/tw-widget.js
@@ -1,5 +1,13 @@
+/*
+ * @name twitch-widget
+ * @author eden lane
+ * @require jQuery
+ */
+
 (function ($) {
   $(".twitch-widget").each(function () {
-    var $w = $(this);
+    var $w = $(this),
+        channel = $w.attr("data-twitch-channel");
+
   })
 })(jQuery);
