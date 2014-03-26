4e6aa94e1abde8b13cec9206e411046e351cd1f3
edencore <edencore@gmail.com>
2014-03-26 19:01:26 +0400

    added gulp tasks and build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    less = require('gulp-less');
 
-gulp.task('default', function () {
+gulp.task('scripts', function () {
+  return gulp.src('src/tw-widget.js')
+      .pipe(uglify())
+      .pipe(rename('tw-widget.min.js'))
+      .pipe(gulp.dest('build'));
+});
 
-})
+gulp.task('styles', function (){
+  return gulp.src('src/tw-widget.less')
+      .pipe(less())
+      .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['scripts', 'styles']);
